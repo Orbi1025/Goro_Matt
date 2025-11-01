b3ea586e82496fb677413ef219963834700ddada
Orbi1025 <john941025@gmail.com>
2025-11-01 16:06:58 -0400

    first commit

---
+++ _assets/7ab3af80c73d0106.js
@@ -0,0 +1,34 @@
+(self["webpackChunk_canva_web"] = self["webpackChunk_canva_web"] || []).push([
+  [71623],
+  {
+    /***/ 478956: function (_, __, __webpack_require__) {
+      __webpack_require__.n_x = __webpack_require__.n;
+      const __web_req__ = __webpack_require__;
+      __web_req__(905716);
+      self._5f74ec40302898c5a55451c9fbd04240 =
+        self._5f74ec40302898c5a55451c9fbd04240 || {};
+      (function (__c) {
+        __c.rGb = {
+          config: {
+            language: "pt-BR",
+            xf: {
+              yMMMd: 'd "de" MMM "de" yyyy',
+              yMd: "dd/MM/yyyy",
+              yMMM: 'MMM "de" yyyy',
+            },
+            Ef: "jan. fev. mar. abr. mai. jun. jul. ago. set. out. nov. dez.".split(
+              " "
+            ),
+            Ff: "janeiro fevereiro mar\u00e7o abril maio junho julho agosto setembro outubro novembro dezembro".split(
+              " "
+            ),
+            ig: "dd *[./\\s-] *mm *[./\\s-] *yy;dd *[./\\s-] *mm *[./\\s-] *yyyy;yyyy *[./\\s-] *mm *[./\\s-] *dd;dd *[./\\s-] *mm;mmm de yyyy;dd de mmm de yyyy".split(
+              ";"
+            ),
+          },
+        };
+      }.call(self, self._5f74ec40302898c5a55451c9fbd04240));
+    },
+  },
+]);
+//# sourceMappingURL=sourcemaps/7ab3af80c73d0106.js.map
